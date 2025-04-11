7e4df58ad7e3ca2b3d5acd39fd1f8df642903752
Annish1234 <annishjk01@gmail.com>
2025-04-11 22:19:40 +0530

    added test case

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,54 @@
-def test_placeholder():
-    assert True 
-#test case modified1
+# def test_placeholder():
+#     assert True 
+# #test case modified1
+
+import pytest
+from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from api.app import app
+
+client = TestClient(app)
+
+sample_input = [
+    {
+        "Time": 151029.0,
+        "V1": -3.8182137183,
+        "V2": 2.5513376778,
+        "V3": -4.7591583159,
+        "V4": 1.6369669588,
+        "V5": -1.1679000839,
+        "V6": -1.6784133878,
+        "V7": -3.1447324721,
+        "V8": 1.2451055504,
+        "V9": -1.6925413541,
+        "V10": -4.7599313739,
+        "V11": 3.6422569394,
+        "V12": -4.1652023226,
+        "V13": 0.7030698751,
+        "V14": -7.624316314,
+        "V15": -1.4980123996,
+        "V16": -4.07999233,
+        "V17": -6.7171765476,
+        "V18": -1.8875496491,
+        "V19": 1.3567482897,
+        "V20": 0.164453446,
+        "V21": 0.8376854401,
+        "V22": 0.7617121271,
+        "V23": -0.4176942659,
+        "V24": -0.4697124715,
+        "V25": -0.2259342358,
+        "V26": 0.5864152485,
+        "V27": -0.3481074059,
+        "V28": 0.0877768724,
+        "Amount": 10.7
+    }
+]
+
+def test_predict_endpoint():
+    response = client.post("/predict/", json=[sample_input[0]])
+    print("Response JSON:", response.json())  
+    assert response.status_code == 200
+    assert "prediction" in response.json()
